cbee32288eb33218b1471179f18999afbcc04eb6
FRDRK <federicoaugelli@gmail.com>
2024-01-15 16:54:17 +0100

    llm

--- lang.py
+++ lang.py
@@ -4,23 +4,28 @@ from langchain.agents import create_openai_functions_agent, AgentExecutor
 from langchain.tools import tool
 from langchain.pydantic_v1 import BaseModel, Field
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from dotenv import load_dotenv
+import os
 
-openai_api_key = "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"
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+#openai_api_key = "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"
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=1, openai_api_key=openai_api_key)
 
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "Generate realistic data based on the content of the provided file. The file may contain information of multiple users (like 10 or 15) such as api keys, passwords or any other relevant data. Data has to look really, without any lorem ipsum or john doe. Ensure that the output is coherent and resembles authentic information. Output only the generated data, without any additional context or filler."),
-    ("user", "{input}"),
-    MessagesPlaceholder(variable_name="agent_scratchpad"),
-])
+
 
 
 
 def retreive_random_data(input_file: str):
-
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", f"Generate realistic data based on the content of the provided file. The file may contain information of multiple users (like 10 or 15) such as api keys, passwords or any other relevant data. Data has to look really, without any lorem ipsum or john doe. Ensure that the output is coherent and resembles authentic information. Output only the generated data, without any additional context or filler, nor Generated data placeholder. The output has to be in the same syntax of {input_file}"),
+        ("user", "{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ])
+    
     class SearchInput(BaseModel):
-        query: str = Field(description="Useful to retreive information. Input will be a filename")
+        query: str = Field(description=f"Useful to retreive information. Input will be a filename, and the output syntax has to be the same of {input_file}")
 
 
     @tool("search-tool", args_schema=SearchInput, return_direct=False)
@@ -56,4 +61,4 @@ def generate_random_data(input_file: str):
 
 #print(generate_random_data("data.json"))
 
-#print(retreive_random_data("data.md"))
+print(retreive_random_data("data.json"))
